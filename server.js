25ec931f80cced4413ea45908c19d204bd694c70
elleserdonaldson <ellessedon@gmail.com>
2023-01-21 14:49:54 +0000

    imported and using path

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
+const path = require("path");
 
 const routes = require("./controllers");
 
@@ -14,6 +15,8 @@ app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use(routes);
 
 const init = async () => {
