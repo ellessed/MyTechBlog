d8dafe56832eb6098314a6a709919c1b90f261c6
elleserdonaldson <ellessedon@gmail.com>
2023-01-21 14:50:13 +0000

    rendering a basic homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,7 +5,7 @@ const router = Router();
 // here is where I'd put the routes if I had any
 router.get("/", (req, res) => {
   try {
-    res.status(200).json({ message: "Welcome" });
+    res.status(200).render("home");
   } catch (error) {
     res.status(500).json({ error: "Something went wrong" });
   }
