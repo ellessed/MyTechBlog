88cf607191510dbb0f142c2ad61766018af26912
elleserdonaldson <ellessedon@gmail.com>
2023-01-21 13:50:07 +0000

    created db relationship between models

--- models/index.js
+++ models/index.js
@@ -1,13 +1,39 @@
-const User = require('./User');
-const Project = require('./Project');
+const User = require("./User");
+const Post = require("./Post");
+const Comment = require("./Comment");
 
-User.hasMany(Project, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
+//user has many posts with user ID
+User.hasMany(Post, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
 });
 
-Project.belongsTo(User, {
-  foreignKey: 'user_id'
+//post belongs to user
+Post.belongsTo(User, {
+  foreignKey: "user_id",
 });
 
-module.exports = { User, Project };
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+});
+
+//user has many comments with user id
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+//comment belongs to user
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+module.exports = { User, Post, Comment };
+
+//comment belongs to a user
+
+//comment belongs to a post
