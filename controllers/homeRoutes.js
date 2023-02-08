c962001b8cfff681e94a56af8c011f7bb50af8d1
elleserdonaldson <ellessedon@gmail.com>
2023-02-08 00:23:49 +0000

    removed comment

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,6 @@ const { Router } = require("express");
 
 const router = Router();
 
-// here is where I'd put the routes if I had any
 router.get("/", (req, res) => {
   try {
     res.status(200).render("home");
