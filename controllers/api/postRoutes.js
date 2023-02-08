7cd49f9859cf3187b278360d04a5321345123666
ellessed <ellessedon@gmail.com>
2023-02-08 21:05:49 +0000

    post routes created with comments

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -3,6 +3,8 @@ const { User, Post, Comment } = require("../../models");
 
 const router = Router();
 
+// crrud - create, read one, read all,  update delete
+
 router.get("/", async (req, res) => {
   try {
     const myPosts = await Post.findAll();
@@ -13,5 +15,32 @@ router.get("/", async (req, res) => {
     return res.status(500).json({ error });
   }
 });
+
+// get post by id
+router.get("/:id", async (req, res) => {
+  try {
+    // get the ID that I'm sending in the query params
+    const postId = req.params.id
+
+    // find the post by its ID
+    const post = await Post.findByPk(postId)
+
+    // gate statement -- if the POST  can't be found we send back a 404-not found
+    if (!post) res.status(404).json({ message: "Not found!" })
+
+    // otherwise, if my request is true, I send back 200-ok and the actual POST that I requested
+    return res.status(200).json()
+
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error });
+  }
+
+  // create new post
+
+  // edit post 
+
+  // delete post
+})
 
 module.exports = router;
