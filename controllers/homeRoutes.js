c67ee99eb6a622c00568679b3c42d653237a7f5f
elleserdonaldson <ellessedon@gmail.com>
2023-02-22 19:35:55 +0000

    commented out code

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,13 +1,29 @@
 const { Router } = require("express");
 const { Post } = require("../models");
 
-const router = Router();
+// router.get("/", async (req, res) => {
+//try {
+//const allPosts = await Post.findAll();
 
+//const posts = { data: allPosts };
+//res.status(200).render("home", {
+//  posts,
+//});
+//} catch (error) {
+//res.status(500).json({ error: "Something went wrong" });
+// }
+//});
+
+const router = Router();
 router.get("/", async (req, res) => {
   try {
     const allPosts = await Post.findAll();
 
-    const posts = { data: allPosts };
+    //const posts = { data: allPosts };
+
+    const posts = allPosts.map((post) => post.get({ plain: true }));
+
+    console.log(posts);
     res.status(200).render("home", {
       posts,
     });
