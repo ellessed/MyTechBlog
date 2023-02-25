c3924b711c161b0299c8a4b873c0ad60117766ed
elleserdonaldson <ellessedon@gmail.com>
2023-02-25 17:24:31 +0000

    import connection

--- server.js
+++ server.js
@@ -1,19 +1,35 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const path = require("path");
-const helpers = require("./utils/helpers");
+const expressSession = require("express-session");
+const connectSessionSequelize = require("connect-session-sequelize")(
+  expressSession.Store
+);
 
+const helpers = require("./utils/helpers");
 const routes = require("./controllers");
+const db = require("./config/connection");
+const { sequelize } = require("./models/User");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-const hbs = exphbs.create({}); // add helpers here if needed
+const hbs = exphbs.create({ helpers });
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET,
+  saveUninitialized: false,
+  resave: false,
+  rolling: true,
+  store: new connectSessionSequelize({ db }),
+  cookie: { maxAge: 600000 },
+};
 
 // Inform Express.js on which template engine to use
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
+
+app.use(expressSession(sessionOptions));
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -22,6 +38,7 @@ app.use(routes);
 
 const init = async () => {
   try {
+    await sequelize.sync();
     app.listen(PORT, () => {
       console.log(`\nServer running on http://localhost:${PORT}/`);
     });
